fde07ff1aaa9cf09ac86f974a9b1c0aaaba1dbad
martincastroc <martcastro@alumnos.uai.cl>
2022-09-26 19:09:35 -0300

    Fixed spelling mistakes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           ¿Crees que es muy difícil encontrar el mejor precio para tus compras?
         </p>
-        <p>Un grupo de universitarios está preparando la solucion para tí</p>
+        <p>Un grupo de universitarios está preparando la solución para ti</p>
         <p>¡Inscríbete a continuación si te interesa!</p>
         <br/>
         <i class="arrow"></i>
